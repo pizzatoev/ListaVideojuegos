fe30f637753367c2599ebdcf805e1861a7f745f6
Pizzatoev <evatoronogales@gmail.com>
2025-05-26 01:43:28 -0400

    Mostrar videojuegos en la ruta principal

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideojuegoController;
 
+// Ruta principal: lista de videojuegos
+Route::get('/', [VideojuegoController::class, 'index']);
+
+// Ruta explícita para ver la lista (opcional)
 Route::get('/videojuegos', [VideojuegoController::class, 'index']);
+
+// Ruta para ver el detalle de un videojuego
 Route::get('/videojuegos/{id}', [VideojuegoController::class, 'show']);
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
